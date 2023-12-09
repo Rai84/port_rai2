2a6e87221d000601a1b35a5ac628c8b5ba8e1a5d
Rai Gonçalves <106636812+raiii84@users.noreply.github.com>
2023-12-09 03:27:27 -0300

    dia09/12

--- assets/js/expandir.js
+++ assets/js/expandir.js
@@ -1,18 +1,18 @@
 document.getElementById("expandir").addEventListener("click", function() {
     var expandido = document.getElementById("expandido");
-    var expandir = document.getElementById("expandir");
+    // var expandir = document.getElementById("expandir");
     var icone2 = document.getElementById("icone2");
 
     if (expandido.style.display === "none" || expandido.style.display === "") {
         expandido.style.display = "flex";
         expandido.classList.add("expandido");
         icone2.classList.replace("bi-chevron-compact-right", "bi-chevron-compact-left");
-        expandir.style.left = "calc(70vw - 7px)";
+        // expandir.style.left = "calc(70vw - 7px)";
     } else {
         expandido.style.display = "none";
         expandido.classList.remove("expandido");
         icone2.classList.replace("bi-chevron-compact-left", "bi-chevron-compact-right");
-        expandir.style.left = "32px";
+        // expandir.style.left = "32px";
     }
 });
 
